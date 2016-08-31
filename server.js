73bad357d51af8536e043c21e7ff8b9fa855acbe
ybbarng <ybbarng@gmail.com>
2016-08-31 20:01:22 +0900

    세미콜론을 모두 추가하는 등 lint의 경고를 모두 적용

--- server.js
+++ server.js
@@ -1,28 +1,28 @@
-var ecstatic = require('ecstatic')
+var ecstatic = require('ecstatic');
 var server = require('http').createServer(
   ecstatic({ root: __dirname, handleError: false })
-)
-var p2pserver = require('socket.io-p2p-server').Server
-var io = require('socket.io')(server)
+);
+var p2pserver = require('socket.io-p2p-server').Server;
+var io = require('socket.io')(server);
 
-server.listen(1225, function () {
-  console.log('Listening on 1225')
-})
+server.listen(1225, function() {
+  console.log('Listening on 1225');
+});
 
-io.use(p2pserver)
+io.use(p2pserver);
 
-io.on('connection', function (socket) {
-  socket.on('peer-msg', function (data) {
-    console.log('Message from peer: %s', data)
-    socket.broadcast.emit('peer-msg', data)
-  })
+io.on('connection', function(socket) {
+  socket.on('peer-msg', function(data) {
+    console.log('Message from peer: %s', data);
+    socket.broadcast.emit('peer-msg', data);
+  });
 
-  socket.on('join', function (peerId) {
+  socket.on('join', function(peerId) {
     socket.peerId = peerId;
     console.log('A peer is connected : %s', socket.peerId);
-  })
+  });
 
-  socket.on('disconnect', function () {
+  socket.on('disconnect', function() {
     console.log('A peer is disconnected : %s', socket.peerId);
-  })
-})
+  });
+});
--- src/index.js
+++ src/index.js
@@ -1,33 +1,33 @@
-var Socketiop2p = require('socket.io-p2p')
-var io = require('socket.io-client')
+var Socketiop2p = require('socket.io-p2p');
+var io = require('socket.io-client');
 
-function init () {
-  var socket = io()
-  var opts = {peerOpts: {trickle: false}, autoUpgrade: false}
-  var p2psocket = new Socketiop2p(socket, opts, function () {
-    p2psocket.emit('join', p2psocket.peerId)
-  })
+function init() {
+  var socket = io();
+  var opts = {peerOpts: {trickle: false}, autoUpgrade: false};
+  var p2psocket = new Socketiop2p(socket, opts, function() {
+    p2psocket.emit('join', p2psocket.peerId);
+  });
 
   // Elements
-  var privateButton = document.getElementById('private')
-  var form = document.getElementById('msg-form')
-  var box = document.getElementById('msg-box')
-  var msgList = document.getElementById('msg-list')
+  var privateButton = document.getElementById('private');
+  var form = document.getElementById('msg-form');
+  var box = document.getElementById('msg-box');
+  var msgList = document.getElementById('msg-list');
 
-  p2psocket.on('peer-msg', function (data) {
-    var li = document.createElement('li')
-    li.appendChild(document.createTextNode(data.textVal))
-    msgList.appendChild(li)
-  })
+  p2psocket.on('peer-msg', function(data) {
+    var li = document.createElement('li');
+    li.appendChild(document.createTextNode(data.textVal));
+    msgList.appendChild(li);
+  });
 
-  form.addEventListener('submit', function (e, d) {
-    e.preventDefault()
-    var li = document.createElement('li')
-    li.appendChild(document.createTextNode(box.value))
-    msgList.appendChild(li)
-    p2psocket.emit('peer-msg', {textVal: box.value})
-    box.value = ''
-  })
+  form.addEventListener('submit', function(e, d) {
+    e.preventDefault();
+    var li = document.createElement('li');
+    li.appendChild(document.createTextNode(box.value));
+    msgList.appendChild(li);
+    p2psocket.emit('peer-msg', {textVal: box.value});
+    box.value = '';
+  });
 }
 
-document.addEventListener('DOMContentLoaded', init, false)
+document.addEventListener('DOMContentLoaded', init, false);
