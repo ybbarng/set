fba8f812cba5d293a29245572022b09fd39594b9
ybbarng <ybbarng@gmail.com>
2016-09-06 14:42:04 +0900

    외부 CDN을 사용해서 jquery 추가

--- src/client.js
+++ src/client.js
@@ -2,10 +2,13 @@ var io = require('socket.io-client');
 
 var Card = require('./card.js');
 
+var $ = require('jquery');
+
 var myId = '';
 
 var myPoint = 0;
 var peerPointView = 0;
+console.log($('message'));
 
 function init() {
   var socket = io();
