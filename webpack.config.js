b0e5349254752a60a5130c0640886bfcf2cb2208
ybbarng <ybbarng@gmail.com>
2019-06-07 15:41:10 +0900

    update outdated

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,6 @@
 const path = require('path');
 const eslintFormatter = require('eslint-formatter-friendly');
-const CleanWebpackPlugin = require('clean-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
